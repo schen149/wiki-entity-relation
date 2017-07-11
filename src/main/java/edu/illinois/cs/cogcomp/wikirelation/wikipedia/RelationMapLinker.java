d578805a17a81128a1bfe37fd90c8de388ab650b
schen149 <schen149@illinois.edu>
2017-07-10 21:08:30 -0500

    rewrite to increase performance

--- src/main/java/edu/illinois/cs/cogcomp/wikirelation/wikipedia/RelationMapLinker.java
+++ src/main/java/edu/illinois/cs/cogcomp/wikirelation/wikipedia/RelationMapLinker.java
@@ -239,26 +239,19 @@ public class RelationMapLinker {
                 }
             }
         }
-
+        Set<Integer> inputPageIds = Arrays.stream(pageIds).boxed().collect(Collectors.toSet());
         List<Map.Entry<Integer, Pair<Integer, Integer>>> sortedCands = new ArrayList<>(cands.entrySet());
 
-        sortedCands = sortedCands.stream()
-                .filter(c -> c.getValue().getFirst() == pageIds.length)
-                .collect(Collectors.toList());
-
-        sortedCands.sort((c1, c2) -> {
-            int relatedPageNum1 = c1.getValue().getFirst();
-            int relatedPageNum2 = c2.getValue().getFirst();
-            if (relatedPageNum1 != relatedPageNum2)
-                return relatedPageNum2 - relatedPageNum1;
-            else
-                return c2.getValue().getSecond() - c1.getValue().getSecond();
-        });
-
-        Set<Integer> inputPageIds = Arrays.stream(pageIds).boxed().collect(Collectors.toSet());
-
-        // Get the candidates ids and filter out the ones already in the input
         int[] candIds = sortedCands.stream()
+                .filter(c -> c.getValue().getFirst() == pageIds.length)
+                .sorted((c1, c2) -> {
+                    int relatedPageNum1 = c1.getValue().getFirst();
+                    int relatedPageNum2 = c2.getValue().getFirst();
+                    if (relatedPageNum1 != relatedPageNum2)
+                        return relatedPageNum2 - relatedPageNum1;
+                    else
+                        return c2.getValue().getSecond() - c1.getValue().getSecond();
+                })
                 .mapToInt(Map.Entry::getKey)
                 .filter(c -> !inputPageIds.contains(c))
                 .toArray();
