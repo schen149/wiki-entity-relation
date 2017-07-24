08670cd58a9ad2d4c0707896eba8036f027e1805
schen149 <schen149@illinois.edu>
2017-07-23 21:36:02 -0500

    ambitious goal: rewrite everything

--- src/main/java/edu/illinois/cs/cogcomp/wikirelation/importer/wikipedia/CooccuranceMapImporter.java
+++ src/main/java/edu/illinois/cs/cogcomp/wikirelation/importer/wikipedia/CooccuranceMapImporter.java
@@ -16,6 +16,7 @@ import org.xml.sax.SAXException;
 
 import java.io.*;
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 /**
@@ -45,12 +46,12 @@ public class CooccuranceMapImporter {
         if (!(new File(dumpdir).exists()))
             new File(dumpdir).mkdir();
         dumpfile = dumpdir + File.separator + language + "wiki-" + date + "-pages-articles.xml.bz2";
-        cooccurfile = dumpdir + "/cooccurance/cooccurance.txt";
+        cooccurfile = dumpdir + "/cooccurance/entities.txt";
     }
 
     private void parseWikiDump() throws IOException, SAXException {
         logger.info("Parsing wikidump " + dumpfile);
-        StringBuilder cand_pair = new StringBuilder();
+        StringBuilder entities = new StringBuilder();
 
         MLWikiDumpFilter filter = new MLWikiDumpFilter(56) {
             @Override
@@ -62,24 +63,28 @@ public class CooccuranceMapImporter {
                 if (page.getTitle().contains("/"))
                     return;
 
-                Integer pageId = Integer.parseInt(page.getId());
-
                 SpanLabelView wikiView = (SpanLabelView) ta.getView(ViewNames.WIKIFIER);
 
-                for (Constituent c : wikiView.getConstituents()) {
-                    Integer hrefPageId = idLinker.getIDFromTitle(c.getLabel());
+                String entityIds = wikiView.getConstituents().stream()
+                        .map(Constituent::getLabel)
+                        .filter(s -> !s.isEmpty())
+                        .filter(title -> WikiUtil.isTitleNEType(title, "en"))
+                        .map(title -> idLinker.getIDFromTitle(title))
+                        .filter(Objects::nonNull)
+                        .map(i -> i.toString())
+                        .collect(Collectors.joining("\t"));
 
-                    if (hrefPageId == null) continue;
-                    synchronized (cand_pair) {
-                        cand_pair.append(pageId+"\t"+hrefPageId+"\n");
+                synchronized (entities) {
+                    if (!entityIds.isEmpty()) {
+                        entities.append(entityIds).append("\n");
 
-                        if (cand_pair.length() > 100000) {
+                        if (entities.length() > 100000) {
                             BufferedWriter bw1 = new BufferedWriter(new FileWriter(cooccurfile, true));
-                            bw1.write(cand_pair.toString());
+                            bw1.write(entities.toString());
                             bw1.close();
-                            cand_pair.delete(0, cand_pair.length());
+                            entities.delete(0, entities.length());
                         }
-                     }
+                    }
                 }
             }
         };
@@ -133,11 +138,11 @@ public class CooccuranceMapImporter {
 
     public static void main(String args[]) {
         CooccuranceMapImporter rmg = new CooccuranceMapImporter("/media/evo/data/wiki/enwiki-20170601/",
-                "20170601", "en", "/home/squirrel/project/wiki-entity-relation/config/sihaopc-tmp.properties");
+                "20170601", "en", "/home/squirrel/project/wiki-entity-relation/config/sihaopc.properties");
 
         try{
-            rmg.populateDB();
-//            rmg.parseWikiDump();
+//            rmg.populateDB();
+            rmg.parseWikiDump();
         }
         catch (Exception e) {
             logger.info("Error reading wikipedia dump at /media/evo/data/wiki/enwiki-20170601/");
